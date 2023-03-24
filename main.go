46deeef5ceb86d717e5dbf9e1414ef211c4a1a57
Tus1688 <76937231+Tus1688@users.noreply.github.com>
2023-03-24 18:32:32 +0700

    feat(core): readfile and core struct of args

--- main.go
+++ main.go
@@ -1,16 +1,27 @@
 package main
 
-import "flag"
+import (
+	"flag"
+
+	"github.com/Tus1688/wordmunger/pkg/wordmunger"
+)
 
 func main() {
 	flag.Usage = func() {
-		println("Usage: -f <input file> -o <output file>")
+		println("Usage: -f <input file> -o <output file> -c <concurrent>")
 	}
-	flag.Parse()
 	inputFile := flag.String("f", "", "input file")
 	outputFile := flag.String("o", "", "output file")
+	concurrent := flag.Int("c", 20, "concurrent")
+	flag.Parse()
 	if *inputFile == "" || *outputFile == "" {
 		flag.Usage()
 		return
 	}
+	arg := wordmunger.WordMunger{
+		InputFile:  *inputFile,
+		OutputFile: *outputFile,
+		Concurrent: *concurrent,
+	}
+	arg.ReadFile()
 }
--- pkg/wordmunger/main.go
+++ pkg/wordmunger/main.go
@@ -1,1 +1,41 @@
 package wordmunger
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+type WordMunger struct {
+	WordTarget []string
+	Concurrent int
+	InputFile  string
+	OutputFile string
+}
+
+func (wm *WordMunger) ReadFile() {
+	curdir, err := os.Getwd()
+	if err != nil {
+		log.Print(err)
+	}
+	var filePath string
+	if len(wm.InputFile) < len(curdir) {
+		filePath = curdir + "/" + wm.InputFile
+	} else {
+		filePath = wm.InputFile
+	}
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Print(err)
+	}
+	defer file.Close()
+	// for each line in file add it to WordTarget slice
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		// check if the line is not empty then add it to WordTarget slice
+		if len(scanner.Text()) > 0 {
+			wm.WordTarget = append(wm.WordTarget, scanner.Text())
+		}
+	}
+}
